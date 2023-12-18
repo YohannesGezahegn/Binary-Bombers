ecbad83ee9db436b191421ceba3ef6154f749c5d
YohannesGezahegn <149233041+YohannesGezahegn@users.noreply.github.com>
2023-12-18 10:05:16 +0300

    Update total_income_calculater.cpp

--- ETS1437_15_yohannes_gezahegn/income_calculater.cpp
+++ ETS1437_15_yohannes_gezahegn/income_calculater.cpp
@@ -1,37 +1,57 @@
-#include<iostream>
+
+#include <iostream>
 using namespace std;
-int main()
+int main ()
 {
-    float gross_salary,overtime_bonus_rate,pension,income_tax,bonus_payment,overtime_payment,net_salary;
-    int worked_hours;
-    float PR=0.07;//PR is pension rate
-    cout<<"enter your gross salary"<<endl;
-    cin>>gross_salary;
-    cout<<"enter your worked hours"<<endl;
-    cin>>worked_hours;
-    cout<<"enter overtime bonus rate"<<endl;
-    cin>>overtime_bonus_rate;
-    pension=gross_salary*PR;
-    overtime_payment=(worked_hours-40)*overtime_bonus_rate;
-    if(gross_salary<=200){
-        income_tax=0;
-    }
-    else if(gross_salary>200&&gross_salary<=600){
-        income_tax=gross_salary*0.1;
-    }
-    else if(gross_salary>600&&gross_salary<=1200){
-        income_tax=gross_salary*0.15;
-    }
-    else if(gross_salary>1200&&gross_salary<=2000){
-        income_tax=gross_salary*0.2;
-    }
-    else if(gross_salary>2000&&gross_salary<3500){
-        income_tax=gross_salary*0.25;
-    }
-    else{
-        income_tax=gross_salary*0.3;
-    }
-    net_salary=(gross_salary-pension-income_tax)+overtime_payment;
-    cout<<"your net salary is"<<net_salary<<endl;
+    float WWHours,DNo,GPay,HI,NPay;
+    char Choice;
+    cout<<"Please enter your Weekly Working Hour"<<endl;
+    cin>>WWHours;
+    if(WWHours>40)
+        {
+            GPay=671.2+(WWHours-40)*25.17;
+        }
+    else
+        {
+            GPay=WWHours*16.78;
+        }
+    NPay=(GPay-(GPay*0.25))-10;
+    cout<<"Please enter umber of dependents"<<endl;
+    cin>>DNo;
+    if(DNo>=3)
+        {
+            HI=35;
+            NPay=NPay-HI;
+        }
+        else
+        {
+            HI=0;
+            NPay=NPay;
+        }
+    cout<<"Which Information about your pay do you want to see?"<<endl<<"Gross pay(1)"<<endl;
+    cout<<"Social Security Tax(2)"<<endl<<"Federal Income Tax(3)"<<endl;
+    cout<<"State Income Tax(4)"<<endl<<"Union Dues(5)"<<endl;
+    cout<<"Health Insurance(6)"<<endl<<"Your Net take-home pay(7)"<<endl;
+    cout<<"Choose the number in the bracket"<<endl;
+    cin>>Choice;
+    switch(Choice)
+        {
+            case'1':  cout<<"Your Gross pay is:  "<<GPay;
+                        break;
+            case'2':    cout<<"Your Social Security Tax is:  "<<GPay*0.06;
+                        break;
+            case'3':    cout<<"Your Federal Income Tax is:  "<<GPay*0.14;
+                        break;
+            case'4':    cout<<"Your State Income Tax is:  "<<GPay*0.05;
+                        break;
+            case'5':    cout<<"Union Dues:  10";
+                        break;
+            case'6':    cout<<"Health Insurance:  "<<HI;
+                        break;
+            case'7':    cout<<"Your Net take-home pay is:  "<<NPay;
+                        break;
+            default:    cout<<"Invalid input";
+                        break;
+        }
     return 0;
 }
